f9ee82e00cb502986c488add4e060c4f491ddfd0
Vasco Gomes <up201906617@fe.up.pt>
2021-11-12 16:29:50 +0000

    output scanner

--- BigNumber.hs
+++ BigNumber.hs
@@ -1,9 +1,53 @@
 --2.1 duvida nesta
 type BigNumber = [Int]
---2.2 --ver a trasnformaçãoi de char para int e ver casos de erro
+--2.2 Era nice ter um limpador para tirar os 0 a esquerda no fim
 scanner :: String -> BigNumber
 scanner []=[]
-scanner (x:xs) = x:(scanner xs)
+scanner (x:[]) = [charToInt x]
+scanner (x:y:xs)
+  | x == '-' = (-1)*(charToInt y):(scanner' xs)
+  | otherwise = charToInt x:(scanner' (y:xs))
+
+scanner' :: String -> BigNumber
+scanner' [] = []
+scanner' (x:xs) = charToInt x:(scanner' xs)
+
+charToInt :: Char -> Int
+charToInt '0' = 0
+charToInt '1' = 1
+charToInt '2' = 2
+charToInt '3' = 3
+charToInt '4' = 4
+charToInt '5' = 5
+charToInt '6' = 6
+charToInt '7' = 7
+charToInt '8' = 8
+charToInt '9' = 9
+charToInt _ = error("Erro a converter um carácter num número")
+
+--2.3
+output :: BigNumber -> String
+output [] = []
+output (x:xs)
+  | x < 0 = '-':(intToChar (abs x)):(output' xs)
+  | otherwise = intToChar x:(output' xs)
+
+output' :: BigNumber -> String
+output' [] = []
+output' (x:xs) = intToChar x:(output' xs)
+
+intToChar :: Int -> Char
+intToChar 0 = '0'
+intToChar 1 = '1'
+intToChar 2 = '2'
+intToChar 3 = '3'
+intToChar 4 = '4'
+intToChar 5 = '5'
+intToChar 6 = '6'
+intToChar 7 = '7'
+intToChar 8 = '8'
+intToChar 9 = '9'
+intToChar _ = error("Erro a converter um número num carácter")
 
 --2.4
 somaBN :: BigNumber -> BigNumber -> BigNumber
--- fib.hs
+++ fib.hs
@@ -1,22 +1,24 @@
 {-1.1 Função recursiva
+    F0 = 1
     F1 = 1
-    F2 = 1
     Fn = Fn-1 + Fn-2
 -}
 fibRec :: (Integral a) => a -> a
+fibRec 0 = 0
 fibRec 1 = 1
-fibRec 2 = 1
 fibRec n = fibRec (n-1) + fibRec (n-2)
 
 {-1.2 Função com programação dinámica
+  lista !! 0 = 0
+  lista !! 1 = 1
+  lista !! 2 = (lista !! 1) + (lista !! 0) = 0 + 1 = 1
+  lista !! i = (lista !! i-1 ) + (lista !! i-2)
 -}
 fibLista :: Int -> Int
-fibLista 1 = 1
-fibLista 2 = 1
-fibLista i = lista !! i where lista = [fibLista (i-1) + fibLista(i-2) | x<-[0..i]]
+fibLista i = lista !! i where lista = 0:1:[lista !! (x-1) + (lista !! (x-2)) | x<-[2..i]]
 
 {-1.3 Função com lista infinita
-    [F1,F2,F1+F2,...,Fi-1 + Fi-2]
+    [F0,F1,F1+F0,...,Fn-1 + Fn-2,...]
 -}
 fibListaInfinita :: Int -> Int
 fibListaInfinita n = lista !! n where lista = 0:1:(zipWith(+) lista (tail lista))
