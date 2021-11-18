3a76087bdd33e5d745f6668ea2c27fd8914fb892
Vasco Gomes <up201906617@fe.up.pt>
2021-11-18 08:31:48 +0000

    bignumber testes

--- BigNumber.hs
+++ BigNumber.hs
@@ -1,3 +1,7 @@
+--1 - cada soluçao tem de ser mais eficiente que a anterior, testar casos limites, 5+5 = 10, 5-6=-1, criar um ficheiro auxiliar com testes usando IO()
+--2 atençao aos numeros negativos
+-- zip aos ficheiros e nao a pasta externa
+
 --2.1 duvida nesta
 type BigNumber = [Int]
 --2.2 Era nice ter um limpador para tirar os 0 a esquerda no fim
@@ -62,8 +66,18 @@ somaBNAux (x:xs) (y:ys)
 
 --2.5
 
+--casos teste, 6-5 = 1, 5-6 = -1, 15-6 = 9, 6-15 = -9, 25-6 = 19, 6-25 = -19, 25-26=-1 (falta confirmar com a subbnteste)
+-- a ideia é se o 2ºBN for maior que o primeiro BN, fazer a conta ao contrario e acrescentar um -
+-- casos a falhar: 25-(-25) (funciona com subbnteste)  [-5] - [9] = [-14]? falta confirmar na soma se x+y-10 é menor que -9
+-- A - B = A + (-B) :D (subbnteste)
+
+subBnTeste :: BigNumber -> BigNumber -> BigNumber
+subBnTeste x y = somaBN x (adicionaSinal y)
+
 subBN :: BigNumber -> BigNumber -> BigNumber
-subBN x y = removeZero (reverse(subBNAux (reverse x) (reverse y)))
+subBN x y
+  | maiorQue x y = removeZero $ reverse $ subBNAux (reverse x) (reverse y)
+  | otherwise = adicionaSinal $ removeZero $ reverse $ subBNAux (reverse y) (reverse x)
 
 subBNAux :: BigNumber -> BigNumber -> BigNumber
 subBNAux x [] = x
@@ -74,7 +88,24 @@ subBNAux (x:xs) (y:ys)
 
 removeZero :: BigNumber -> BigNumber
 removeZero (x:xs)
+  | xs == [] = [x]
   | x==0 = removeZero xs
   | otherwise = (x:xs)
+
+maiorQue :: BigNumber -> BigNumber -> Bool
+maiorQue (x:xs) (y:ys)
+  | lenX > lenY = True
+  | lenY > lenX = False
+  | x > y = True
+  | y > x = False
+  | x == y && xs == [] = False
+  | x == y && x < 0 = maiorQue ys xs
+  | otherwise = maiorQue xs ys
+  where
+    lenX = length (x:xs)
+    lenY = length (y:ys)
+
+adicionaSinal :: BigNumber -> BigNumber
+adicionaSinal (x:xs) = (-x:xs)
 
 --2.6
