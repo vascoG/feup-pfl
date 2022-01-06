4a11b350994bc658b5d8b1fec6e5e502e40c7270
Vasco Gomes <up201906617@fe.up.pt>
2022-01-06 10:43:25 +0000

    jogada validada e executada

--- pfl2/board.pl
+++ pfl2/board.pl
@@ -1,3 +1,5 @@
+:- use_module(library(lists)).
+
 /**
 * Board is represented by a list of lists that can be queried using predicate board(+Board,+I-J,-Player), meaning in position (I,J) there is
 * a cube placed by the Player. If there is no cubes, Player returns 0.
@@ -10,7 +12,7 @@ initial_state(Size,gamestate(Board,1,nan-nan)):- initial_board(Size, Board).
 initial_board(Size,Board):- 
     create_board(Size,Board,Size).
 
-create_board(Size,[],0).
+create_board(_,[],0).
 
 create_board(Size,[Line|Board],Column):-
     Column > 0,
@@ -29,6 +31,20 @@ create_line(Size, Line):-
     Size1 is Size-1,
     create_line(Size1,Line1),
     append(Line1,[0],Line).
+
+%board(+Board,+I-J,?Player)
+board(Board,I-J,Player):-
+    nth0(I, Board, Line),
+    nth0(J, Line, Player).
+
+%update_board(+Board, +I, +J, +Player, -NewBoard)
+update_board([[_|Line]|Board],0,0,Player,[[Player|Line]|Board]):- !.
+update_board([[E|Line]|Board],0,J,Player,[[E|NewLine]|Board]):- 
+    J1 is J-1,
+    update_board([Line|Board],0,J1,Player,[NewLine|Board]).
+update_board([Line|Board],I,J,Player,[Line|NewBoard]):- 
+    I1 is I-1,
+    update_board(Board,I1,J,Player, NewBoard).
 
 
 
--- pfl2/move.pl
+++ pfl2/move.pl
@@ -1,4 +1,49 @@
+:- ensure_loaded(board).
+
 /**
 * Game state is represented by the structure gamestate(Board, Turn, I-J), where -(I,J)
 * represents the last cube placed by opponent.
 */
+
+%move(+GameState, +Move, -NewGameState)
+move(gamestate(Board,Turn,I-J), MoveI-MoveJ, gamestate(NewBoard,NewTurn, MoveI-MoveJ)):-
+    length(Board, Size), %Obter tamanho do tabuleiro e verificar se a jogada está dentro do tabuleiro
+    MoveI >= 0,
+    MoveJ >= 0,
+    MoveI < Size,
+    MoveJ < Size,
+    is_adjacent(MoveI-MoveJ, I-J), %Verificar se a jogada é adjacente à última jogada
+    board(Board, MoveI-MoveJ, 0),  %Verificar se a célula está vazia.
+    Turn1 is (Turn rem 2), 
+    player_turn(Turn1, Player),
+    update_board(Board,MoveI,MoveJ,Player, NewBoard),  %Atualiza o tabuleiro
+    NewTurn is Turn+1.
+
+
+%is_adjacent(+I1-J1,+I2-J2)
+is_adjacent(_, nan-nan):- !.
+
+is_adjacent(I1-J1,I2-J2):- 
+    J1 is J2+1,
+    I1 is I2+1, !.
+is_adjacent(I1-J1,I2-J2):-
+    J1 is J2-1,
+    I1 is I2-1, !.
+is_adjacent(I1-J1,I2-J2):-
+    J1 is J2+1,
+    I1 is I2-1, !.
+is_adjacent(I1-J1,I2-J2):-
+    J1 is J2-1,
+    I1 is I2+1, !.
+is_adjacent(I1-J1,I2-J1):-
+    I1 is I2+1, !.
+is_adjacent(I1-J1,I2-J1):-
+    I1 is I2-1, !.
+is_adjacent(I1-J1,I1-J2):-
+    J1 is J2+1, !.
+is_adjacent(I1-J1,I1-J2):-
+    J1 is J2-1.
+
+%player_turn(+Turn, -Player)
+player_turn(0, 2).
+player_turn(1, 1).
